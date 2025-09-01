0de840ef23f06275e130a028ca588087711810cf
Hawkynt <github@synthelicz.de>
2025-09-01 12:01:43 +0200

    + unittest logs

--- make.bat
+++ make.bat
@@ -1,13 +1,30 @@
+@ECHO OFF
 SET LOGFILE=Make.log
 VER >%LOGFILE%
 DATE /D >>%LOGFILE%
 TIME /D >>%LOGFILE%
-FOR %%A IN (*.BAS) DO PBC.EXE -Fnpx -G386 -OZF+ -Ce %%A>>%LOGFILE%
+
+IF EXIST UNITTEST.EXE GOTO ClearOldUnitTest
+GOTO NoOldUnitTest
+:ClearOldUnitTest
+ECHO [Info] Deleting old Unit Test Executable
+ECHO [Info] Deleting old Unit Test Executable >>%LOGFILE%
+DEL UNITTEST.EXE
+DEL UNITTEST.LOG
+:NoOldUnitTest
+
+FOR %%A IN (*.BAS) DO PBC.EXE -FNPX -G386 -ODV -OZF+ -CE -ES -EB -LB -LG %%A>>%LOGFILE%
+
+ECHO [Info] Compilation Log:
 TYPE %LOGFILE%
+
 IF EXIST UNITTEST.EXE GOTO RunUnitTest
 GOTO End
 :RunUnitTest
+ECHO [Info] Found Unit Test Executable
 ECHO [Info] Found Unit Test Executable >>%LOGFILE%
 UNITTEST.EXE >>%LOGFILE%
+TYPE UNITTEST.LOG
+TYPE UNITTEST.LOG >>%LOGFILE%
 
 :End
